8764c9d9d2a9c97b6bc222c3f8f4d2d2e3d1c1ce
Kirill Klimuk <kklimuk@gmail.com>
2013-12-22 17:11:39 -0500

    updates to parser

--- parser.py
+++ parser.py
@@ -1,91 +1,90 @@
 import dataset
 import requests
+import os
 
 from bs4 import BeautifulSoup
 from time import sleep
 from uuid import uuid4
 from datetime import datetime
 
-def get_item_from_link(link):
-    title = link.find('a', 'title')
-    if title is None:
-        return False
 
-    comments = link.find(class_='comments').string.split(' ')[0]
-    return {
-        "title": title.string,
-        "link": title['href'],
-        "rank": int(link.find(class_='rank').string) if link.find(class_='rank').string else False,
-        "upvotes": int(link['data-ups']),
-        "downvotes": int(link['data-downs']),
-        "comments": int(comments) if comments != "comment" else False,
-        "thumbnail": link.select('.thumbnail img')[0]['src'] if len(link.select('.thumbnail img')) > 0 else False,
-        "subreddit": link.find(class_='subreddit').string if link.find(class_='subreddit') else False,
-        "observed": datetime.now(),
-        "reddit_id": filter(lambda x: 'id-' in x, link['class'])[0][3:]
-    }
+def get_document_from_remote(count, after):
+    return BeautifulSoup(requests.get('http://www.reddit.com', params={
+        "limit": 100,
+        "count": count,
+        "after": after
+    }).text)
+
 
 def get_dataset_from_document(document):
     return filter(
-        lambda link: link and link['rank'],
-        map(get_item_from_link, document.find_all(class_='thing'))
+        lambda entry: entry and entry[1]['rank'],
+        map(get_data_from_html_entry, document.find_all(class_='thing'))
     )
 
-def add_thumbnail_to_item(item):
-    link = item['thumbnail']
+
+def get_data_from_html_entry(entry):
+    title = entry.find('a', 'title')
+    if title is None:
+        return False
+
+    comments = entry.find(class_='comments').string.split(' ')[0]
+    subreddit = entry.find(class_='subreddit')
+    thumbnail = entry.select('.thumbnail img')
+    rank = entry.find(class_='rank').string
+
+    return ({
+        "reddit_id": filter(lambda x: 'id-' in x, entry['class'])[0][3:],
+        "title": title.string,
+        "entry": title['href'],
+        "subreddit": subreddit.string if subreddit else False,
+        "thumbnail": thumbnail[0]['src'] if len(thumbnail) > 0 else False
+    }, {
+        "rank": int(rank) if rank else False,
+        "upvotes": int(entry['data-ups']),
+        "downvotes": int(entry['data-downs']),
+        "comments": int(comments) if comments != "comment" else False,
+        "observed": datetime.now()
+    })
+
+
+def add_thumbnail_to_entry(entry):
+    link = entry['thumbnail']
     if not link:
-        item['thumbnail'] = None
-        return item
+        entry['thumbnail'] = None
+        return entry
 
     thumbnail = requests.get(link)
-    item['thumbnail'] = './downloads/%s.%s' % (uuid4(), thumbnail.headers['content-type'].split('/')[1])
-    with open(item['thumbnail'], 'w+') as f:
+    entry['thumbnail'] = './downloads/%s.%s' % (uuid4(), thumbnail.headers['content-type'].split('/')[1])
+    with open(entry['thumbnail'], 'w+') as f:
         f.write(thumbnail.content)
 
-    return item
+    return entry
 
 
-def get_data_from_remote(count, after):
-    document = BeautifulSoup(requests.get('http://www.reddit.com?limit=100&count=%d&after=%s' % (count, after)).text)
-    return get_dataset_from_document(document)
-
-
-def mine():
+def mine(sleep_between=5, sleep_total=550):
     db = dataset.connect('sqlite:///reddit.db')
-    entries = db['entry']
-    statuses = db['status']
+    if not os.path.exists('./downloads'):
+        os.mkdir('./downloads')
 
     while True:
         last_id = "";
-        for x in xrange(0, 1000, 100):
-            data = get_data_from_remote(x, last_id)
-            for item in data:
-                if item == data[-1]:
-                    last_id = item['reddit_id']
+        for count in xrange(0, 1000, 100):
+            data = get_dataset_from_document(get_document_from_remote(count, last_id))
+            for i, (entry, status) in enumerate(data):
+                if i == len(data) - 1:
+                    last_id = entry['reddit_id']
 
-                saved = entries.find_one(reddit_id=item['reddit_id'])
-                if saved is None:
-                    add_thumbnail_to_item(item)
-                    eid = entries.insert({
-                        "reddit_id": item['reddit_id'],
-                        "thumbnail": item['thumbnail'],
-                        "subreddit": item['subreddit'],
-                        "title": item['title'],
-                        "link": item['link']
-                    })
+                saved_entry = db['entry'].find_one(reddit_id=entry['reddit_id'])
+                if saved_entry is None:
+                    status['eid'] = db['entry'].insert(add_thumbnail_to_entry(entry))
                 else:
-                    eid = saved['id']
+                    status['eid'] = saved_entry['id']
+                db['status'].insert(status)
 
-                del item['reddit_id']
-                del item['thumbnail']
-                del item['subreddit']
-                del item['title']
-                del item['link']
+            sleep(sleep_between)
+        sleep(sleep_total)
 
-                item['eid'] = eid
-                statuses.insert(item)
-            sleep(5)
-        sleep(550)
 
 if __name__ == "__main__":
     mine()
