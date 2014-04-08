faa8cf954fd28cfdd4d43f5b4a2a138576f014e8
Timothy Blumberg <tblumberg39@gmail.com>
2014-04-08 14:42:08 -0400

    Building classifying

--- Analysis/classify.py
+++ Analysis/classify.py
@@ -1,6 +1,16 @@
-import sklearn
+from sklearn import naive_bayes
 import sys
-import numpy
-import scipy
+import csv
+# import numpy
+# import scipy
 
-classifer = 
+data = []
+with open('curtable.csv', 'rb') as d:
+	fileReader = csv.reader(d, delimiter=',', quotechar='|')
+
+	for row in fileReader:
+		data.append(row)
+print data
+
+classifer = naive_bayes.BernoulliNB(alpha=1.0)
+classifer.fit(data[0], data[1], data[0])
